14f24a25eff25181af83a0103802e8911d7f6ad7
Md Jashim Uddin <uddin.md.jashim@gmail.com>
2020-12-07 17:23:15 -0500

    fix!: new commit with ! 1.3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Test commit 5.1");
+		System.out.println("Test commit 5.2");
 	}
 
 }
